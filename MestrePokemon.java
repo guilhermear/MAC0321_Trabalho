d3255761c395fbcce02867fd52938f927969690f
guilhermear <guilherme.antonio.rodrigues@usp.br>
2016-04-28 23:23:17 -0300

    Added files via upload

---
+++ MestrePokemon.java
@@ -0,0 +1,39 @@
+package basics;
+
+public class MestrePokemon {
+	private String nome;
+	private Pokemon pokemons[] = new Pokemon[6];
+	private Pokemon pokemonAtual = new Pokemon("Pikachu");
+	
+	public MestrePokemon(String nome){
+		this.nome = nome;
+		if(this.nome == "Ash"){
+			pokemons[0] = new Pokemon("Charmander");
+			pokemons[1] = new Pokemon("Pikachu");
+			pokemons[2] = new Pokemon("Lucario");
+			pokemons[3] = new Pokemon("Chikorita");
+			pokemons[4] = new Pokemon("Squirtle");
+			pokemons[5] = new Pokemon("Hitmonchan");
+		}
+		if(this.nome == "TrAsh"){
+			pokemons[0] = new Pokemon("Abra");
+			pokemons[1] = new Pokemon("Sandslash");
+			pokemons[2] = new Pokemon("Jigglypuff");
+			pokemons[3] = new Pokemon("Gengar");
+			pokemons[4] = new Pokemon("Onix");
+			pokemons[5] = new Pokemon("Trubbish");
+		}
+		if(this.nome == "Pikachu")
+			pokemons[0] = new Pokemon("Pikachu");
+		if(this.nome == "Abra")
+			pokemons[0] = new Pokemon("Abra");
+		if(this.nome == "Onix")
+			pokemons[0] = new Pokemon("Onix");
+	}
+	public Pokemon getPokemon (int indice){
+		return pokemons[indice];
+	}
+	public String getNome(){
+		return this.nome;
+	}
+}
